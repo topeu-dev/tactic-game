d2f12df45b4865c3be1c86a9a7e2bd758e4adeea
topeu <urih.daniil@gmail.com>
2024-12-01 19:44:37 +0100

    upd

--- Assets/Scripts/Actions/TaskExecutor/CharTaskExecutionController.cs
+++ Assets/Scripts/Actions/TaskExecutor/CharTaskExecutionController.cs
@@ -8,27 +8,59 @@ public class CharTaskExecutionController : MonoBehaviour, TaskExecutor
 {
     public void ExecuteTask(ActionInstance actionInstance, ActionContext actionContext, Action callback)
     {
-        switch (actionInstance.actionDescription.actionName)
+        switch (actionInstance.actionDescription.actionVisualizerType)
         {
-            case "Move":
+            case ActionVisualizerType.Move:
+            {
                 gameObject.GetComponent<CharMover>().Move(actionInstance, actionContext, callback);
                 break;
-            case "Hit":
+            }
+            case ActionVisualizerType.MeleeHit:
+            {
                 gameObject.GetComponent<MeleeHitExecutor>().Hit(actionInstance, actionContext, callback);
                 break;
-            case "MeleeAoeHit":
+            }
+            case ActionVisualizerType.MeleeHitAoe:
+            {
                 gameObject.GetComponent<MeleeAoeExecutor>().AoeHit(actionInstance, actionContext, callback);
                 break;
-            case "Raycast":
+            }
+            case ActionVisualizerType.RaycastTargetHit:
+            {
                 gameObject.GetComponent<HitScanExecutor>().HitScan(actionInstance, actionContext, callback);
                 break;
-            case "ApplyRage":
-                gameObject.GetComponent<HitScanExecutor>().HitScan(actionInstance, actionContext, callback);
-                break;  
+            }
             default:
                 Debug.Log("Error: gameObject: " + gameObject.name + " doesn't know how to execute " +
                           actionInstance.actionDescription.actionName);
                 break;
         }
+
+
+        // switch (actionInstance.actionDescription.actionName)
+        // {
+        //     case "Move":
+        //         gameObject.GetComponent<CharMover>().Move(actionInstance, actionContext, callback);
+        //         break;
+        //     case "Hit":
+        //         gameObject.GetComponent<MeleeHitExecutor>().Hit(actionInstance, actionContext, callback);
+        //         break;
+        //     case "MeleeAoeHit":
+        //         gameObject.GetComponent<MeleeAoeExecutor>().AoeHit(actionInstance, actionContext, callback);
+        //         break;
+        //     case "Raycast":
+        //         gameObject.GetComponent<HitScanExecutor>().HitScan(actionInstance, actionContext, callback);
+        //         break;
+        //     case "ApplyRage":
+        //         gameObject.GetComponent<HitScanExecutor>().HitScan(actionInstance, actionContext, callback);
+        //         break;  
+        //     case "Heal":
+        //         gameObject.GetComponent<HitScanExecutor>().HitScan(actionInstance, actionContext, callback);
+        //         break;   
+        //     default:
+        //         Debug.Log("Error: gameObject: " + gameObject.name + " doesn't know how to execute " +
+        //                   actionInstance.actionDescription.actionName);
+        //         break;
+        // }
     }
 }
